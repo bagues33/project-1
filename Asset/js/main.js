95878344cd0311ba442dbc05f686e8624559524e
bagues33 <bagustofan@yahoo.com>
2021-08-30 15:46:32 +0700

    update

---
+++ Asset/js/main.js
@@ -0,0 +1,16 @@
+
+ function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function openjadNav() {
+  document.getElementById("jadSidenav").style.width = "330px";
+}
+
+function closejadNav() {
+  document.getElementById("jadSidenav").style.width = "0";
+}
